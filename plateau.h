351ca224362bcd9a9d12ccaa641a62fef1aed2bb
briac <briac@hirou.org>
2022-12-27 15:53:25 +0100

    ajout de commentaires pour génération automatique de documentation

--- plateau.h
+++ plateau.h
@@ -1,13 +1,18 @@
-//
-// Created by briac on 19/12/2022.
-//
+/**
+ * @file plateau.h
+ * @date 19 décembre 2022
+ * @brief Gestion du plateau du labyrinthe
+ * @author Briac
+*/
 
 #ifndef LABYRINTHE_PLATEAU_H
 #define LABYRINTHE_PLATEAU_H
 
 
-/* Cette structure permet de modifier le tableau d'une case en sauvegardant l'emplacement de la case à faire tourner
- * avant qu'elle ne bouge.*/
+/**
+ * @brief cette structure permet de modifier le tableau d'une case en sauvegardant l'emplacement de la case à faire tourner
+ * avant qu'elle ne bouge.
+ */
 typedef struct sauvegarde_coordonees_mini_case_a_deplacer{
     int ligne1;
     int colonne1;
@@ -17,7 +22,9 @@ typedef struct sauvegarde_coordonees_mini_case_a_deplacer{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//structure contenant toutes les informations sur une case
+/**
+ * @brief structure contenant toutes les informations sur une case.
+ */
 typedef struct Case{
     int ligne;
     int colonne;
@@ -32,7 +39,9 @@ typedef struct Case{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// structure contenant toutes les informations sur un joueur
+/**
+ * @brief structure contenant toutes les informations sur un joueur.
+ */
 typedef struct joueur{
     char nom[15];
     char couleur;
@@ -41,8 +50,10 @@ typedef struct joueur{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-/*Structure contenant simplement des coordonnées dans un tableau. Cela permet plus de lisibilité lors d'appels
- *contenant des coordonnées.*/
+/**
+ * @brief Structure contenant simplement des coordonnées dans un tableau. Cela permet plus de lisibilité lors d'appels
+ * contenant des coordonnées.
+ */
 typedef struct coordonnees_pour_faciliter_les_appels{
     int ligne;
     int colonne;
@@ -50,69 +61,92 @@ typedef struct coordonnees_pour_faciliter_les_appels{
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Ces deux fonctions servent au changement de l'orientation des pièces du labyrinthe
-void deg_rad(float *nb); //convertit les degrés en radians
-void rad_deg(float *nb); //convertit les radians en degrés
-/*
- * Paramètres : pointeur sur un nombre flottant.
- * Retourne : conversion en radians ou conversion en degrés.
+/**
+ * @brief Cette fonction sert au changement de l'orientation des pièces du labyrinthe. Elle convertit les degrés en
+ * radians.
+ *
+ * @param nb pointeur sur un nombre flottant.
+ * @return un angle en radians.
  */
+void deg_rad(float *nb);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Procédure permettant de supprimer la première occurrence d'un caractère dans une chaîne de caractères.
+/**
+ * @brief Cette fonction sert au changement de l'orientation des pièces du labyrinthe. Elle convertit les radians en
+ * degrés.
+ *
+ * @param nb pointeur sur un nombre flottant.
+ * @return un angle en degrés.
+ */
+void rad_deg(float *nb);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @brief Procédure permettant de supprimer la première occurrence d'un caractère dans une chaîne de caractères.
+ *
+ * @param chaine pointeur sur une chaîne de caractères.
+ * @param lettre le caractère à supprimer.
+ * @return chaîne avec une occurence en moins de lettre.
+ */
 void del_1_occ(char *chaine, char lettre);
-/*
- * Paramètres : pointeur sur une chaine de caractères && le caractère à supprimer.
- * Retourne : la chaine avec une occurrence en moins de lettre.
- */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Procédure copiant une structure case dans une autre.
+/**
+ * @brief Procédure copiant une structure case dans une autre.
+ *
+ * @param source pointeur sur une case source.
+ * @param destination pointeur sur une case destination.
+ * @return toutes les données de sources sont dans destination --> destination == source.
+ */
 void copy_case(t_case *source, t_case *destination);
-/*
- * Paramètres : pointeur sur une case source && pointeur sur une case destination.
- * Retourne : toutes les données de sources sont dans destination --> destination == source.
- */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// La procédure suivante crée les tuiles avec leur chemin.
+/**
+ * @brief Procédure créant les tuiles avec leur chemin.
+ *
+ * @param tuile pointeur sur une tuile du labyrinthe.
+ * @return la case a maintenant un type (T, L ou I) et un tableau 3x3 représentant son chemin avec l'orientation.
+ */
 void creation_type_case(t_case* tuile);
-/*
- * Paramètres : pointeur sur une tuile du labyrinthe.
- * Retourne : la case a maintenant un type (T, L ou I) et un tableau 3x3 représentant son chemin avec l'orientation.
- */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// La procédure suivante modifie l'orientation d'une pièce. Cette fontion prend en paramètres le pointeur de la tuile
-// à faire tourner et la nouvelle rotation qu'on souhaite lui appliquer.
+/**
+ * @brief Procédure modifie l'orientation d'une pièce. Cette fontion prend en paramètres le pointeur de la tuile à faire tourner et la nouvelle rotation qu'on souhaite lui appliquer.
+ *
+ * @param tuile pointeur sur la tuile du labyrinthe à faire tourner.
+ * @param orientation flottant correspondant nouvelle rotation qu'on veut lui appliquer.
+ * @return la case a maintenant une nouvelle orientation
+ * @return un nouveau tableau 3x3.
+ */
 void tourner(t_case* tuile, float orientation);
-/*
- * Paramètres : pointeur sur la tuile du labyrinthe à faire tourner && la nouvelle rotation qu'on veut lui appliquer.
- * Retourne : la case a maintenant une nouvelle orientation && un nouveau tableau 3x3.
- */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//La procédure suivante crée le plateau de jeu avec la tuile additionnelle.
+/**
+ * @brief Procédure crée le plateau de jeu avec la tuile additionnelle.
+ *
+ * @param labyrinthe tableau 7x7 composé de structures t_case.
+ * @param tuile_add pointeur sur la tuile en trop du labyrinthe.
+ * @return toutes les cases du labyrinthe ont leurs métadonnées remplies correctement.
+ */
 void generation_plateau_debut(t_case labyrinthe[7][7], t_case* tuile_add);
-/*
- * Paramètres : un tableau 7x7 composé de structures t_case && pointeur sur la tuile en trop du labyrinthe.
- * Retourne : toutes les cases du labyrinthe ont leurs métadonnées remplies correctement.
- */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-//Cette procédure permet de déplacer une colonne ou une ligne.
-void deplacer_tuiles(t_case labyrinthe[7][7], t_case *tuile_en_plus, t_coord *coord);
-/*
- * Paramètres : le tableau du plateau && pointeur sur la tuile en plus du labyrinthe && les coordonnées de la tuile en
- * bout de ligne ou de colonne qui va se faire "pousser".
- * Retourne : la colonne ou la ligne a bien coulissé. Nous avons donc la tuile en plus qui a changé.
+/**
+ * @brief Procédure permettant de déplacer une colonne ou une ligne.
+ *
+ * @param labyrinthe tableau 7x7 composé de structures t_case.
+ * @param tuile_en_plus pointeur sur la tuile en trop du labyrinthe.
+ * @param coord pointeur sur les coordonnées de la tuile en bout de ligne ou de colonne qui va se faire "pousser".
+ * @return la colonne ou la ligne a bien coulissé. Nous avons donc la tuile en plus qui a changé.
  */
+void deplacer_tuiles(t_case labyrinthe[7][7], t_case *tuile_en_plus, t_coord *coord);
 
 
 #endif //LABYRINTHE_PLATEAU_H
