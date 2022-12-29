e6c9fec4c5ef8d3427511a63a9cf72cba76dc0e5
briac <briac@hirou.org>
2022-12-30 00:15:00 +0100

    déplacement des tests dans une fonction dédiée

---
+++ tests.c
@@ -0,0 +1,45 @@
+//
+// Created by briac on 29/12/2022.
+//
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+#include "plateau.h"
+#include "pion.h"
+
+void tests(){
+    printf("TESTS\n");
+    char liste[] = "AAAABBBBDDDJKSJLJQPPPPQSOJDEENCA";
+    int c;
+    int i,j,u,v;
+    t_case Case;
+    t_case *pt_tuile = &Case;
+    pt_tuile->ligne = 1;
+    pt_tuile->colonne = 1;
+    pt_tuile->forme = 'L';
+    creation_type_case(pt_tuile);
+    pt_tuile->fixe = 1;
+    pt_tuile->tresor = 0;
+    pt_tuile->start_finish = 1;
+    pt_tuile->rotation = 0;
+
+    for(i=0;i<360;i=i+90){
+        for(j=0;j<360;j=j+90){
+            pt_tuile->rotation = (float)i;
+            printf("Orientation ini :%f", pt_tuile->rotation);
+            tourner(pt_tuile, (float)j);
+            printf("\tOrientation nouvelle :%f\n", pt_tuile->rotation);
+
+            for(u=0;u<3;u++){
+                for(v=0;v<3;v++){
+                    printf("%c",pt_tuile->tableau[u][v]);
+                }
+                printf("\n");
+            }
+            printf("\n");
+
+        }
+    }
+
+}
---
+++ tests.h
@@ -0,0 +1,9 @@
+//
+// Created by briac on 29/12/2022.
+//
+
+#ifndef LABYRINTHE_TESTS_H
+#define LABYRINTHE_TESTS_H
+void tests();
+
+#endif //LABYRINTHE_TESTS_H
