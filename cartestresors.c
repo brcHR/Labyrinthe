aa1d3a7d28a49f0547045bc52651aee21ce6e54d
maximevennin <maxime.vennin@hotmail.fr>
2022-12-30 21:51:50 +0100

    Ajout du tableau t_tresor_sur_carte dans pion.h

--- cartestresors.c
+++ cartestresors.c
@@ -3,21 +3,10 @@
 //
 #include "cartestresors.h"
 
-typedef struct { //structure temporaire pour test
-    char signe;
-    int decouvert; // 1 s'il a été découvert,0 sinon
-}t_tresor_sur_carte;
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-typedef struct {
-    //t_case *position_pion; // Pointeur vers la case où le pion se trouve
-    int num_pion;
-    char nom[15];
-    char couleur;
-    int arrivee; // Variable booléenne indiquant si le pion a atteint sa destination
-    int deja_deplace; // Variable booléenne indiquant si le pion a déjà été déplacé dans le tour en cours
-    t_tresor_sur_carte tresors[12];// tableau stockant tous les tresors du joueur, les cases en trop sont des '0' et on en met 12 car on a au max 12 tresors par joueur
-    int num_tresor_pion;//numéro du trésor en cours de recherche
-}t_pion;
 
 void AffichageTresor(t_pion *Pion){
     int cartesatrouver=0;//compte le nombre de cartes restantes à trouver
@@ -118,7 +107,7 @@ void DistributionCartes(const int *nbjoueurs, t_pion *pion1, t_pion *pion2, t_pi
     }
 }
 
-///////à mettre dans le futur main
+///////à mettre dans le futur main ou dans le test
 
 int main(){
     int nbjoueurs;
---
+++ deroulement d'un tour.c
@@ -0,0 +1,12 @@
+//
+// Created by Maxime Vennin on 30/12/2022.
+//
+
+
+void deroulementtour(int *nbjoueurs){
+    int numjoueurencours=0;// de 0 à 3  c'est le numéro du joueur qui est entrain de jouer
+    switch (*nbjoueurs) {
+        case 2:
+            
+    }
+}
